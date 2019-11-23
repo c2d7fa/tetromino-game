680a8282ddebe6f376d6dfe3a5d4cad62825ce74
c2d7fa <c2d7fa@gmail.com>
2019-11-23 22:57:50 +0100

    Convert indentation to spaces and reorganize scripts

---
+++ Scripts/Main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const single_tile_scene = preload("res://Scenes/SingleTileGrey.tscn")
+const tile_width = 32
+
+func _add_tile_at(x, y):
+  var tile = single_tile_scene.instance()
+  tile.position = Vector2(x, y)
+  tile.name = "Tile"
+  self.add_child(tile)
+
+func _ready():
+  for x in range(0, 10):
+    for y in range(0, 10):
+      _add_tile_at(x * tile_width, y * tile_width)
