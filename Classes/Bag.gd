4174cfee5cf7e35506f8db8a81c943e608e3ca8c
c2d7fa <c2d7fa@gmail.com>
2019-11-30 15:27:58 +0100

    Implement randomness using a "bag" algorithm

---
+++ Classes/Bag.gd
@@ -0,0 +1,12 @@
+const Piece = preload("res://Classes/Piece.gd")
+
+var _bag = []
+
+func draw():
+  if _bag.empty():
+    _refill_bag()
+  return _bag.pop_front()
+
+func _refill_bag():
+  _bag = [Piece.PieceType.I, Piece.PieceType.O, Piece.PieceType.T, Piece.PieceType.J, Piece.PieceType.L, Piece.PieceType.S, Piece.PieceType.Z]
+  _bag.shuffle()
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 const Piece = preload("res://Classes/Piece.gd")
 const GameOverScene = preload("res://Scenes/GameOverScene.tscn")
+const Bag = preload("res://Classes/Bag.gd")
 
 var piece: Piece
 
 var piece_type = 0
+var bag = Bag.new()
 
 func lose():
   get_tree().change_scene_to(GameOverScene)
 
 func _new_piece():
-  piece_type = (piece_type + 1) % Piece.PieceType.size()
-  piece = Piece.new(piece_type, $Board, self, $MoveTimer)
+  piece = Piece.new(bag.draw(), $Board, self, $MoveTimer)
   piece.connect("placement", self, "_on_placement")
   add_child(piece)
 
