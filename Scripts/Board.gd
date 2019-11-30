f7ebf65396bac66c382a1e0d064f72ca1d9bd729
c2d7fa <c2d7fa@gmail.com>
2019-11-30 14:22:47 +0100

    Add line clearing

--- Scripts/Board.gd
+++ Scripts/Board.gd
@@ -11,8 +11,9 @@ const height = 18
 var _filled_tiles = []
 
 func place_piece(piece):
-  for offset in piece.get_filled_offsets():
-    _fill_tile(piece.x + offset[0], piece.y + offset[1], piece.color())
+  for tile in piece.get_real_tile_positions():
+    _fill_tile(tile[0], tile[1], piece.color())
+  _try_clear()
 
 func is_filled(x, y):
   for pos in _filled_tiles:
@@ -30,10 +31,44 @@ func _fill_tile(x, y, color):
   tile.modulate.b8 = color[2]
   add_child(tile)
 
+func _is_row_filled(r):
+  for c in width:
+    if not is_filled(c, r):
+      return false
+  return true
+
+func is_background_tile(node):
+  return node.name.begins_with("Background") or node.name.begins_with("@Background@")
+
+func _clear_row(r):
+  # Clear this row
+  for child in get_children():
+    if child.position.y == r * tile_width && !is_background_tile(child):
+      remove_child(child)
+  var filled_tiles = []
+  for tile in _filled_tiles:
+    if tile[1] != r:
+      filled_tiles.append(tile)
+  _filled_tiles = filled_tiles
+
+  # Move everything above it down by one
+  for child in get_children():
+    if child.position.y < r * tile_width && !is_background_tile(child):
+      child.position.y += tile_width
+  for tile in _filled_tiles:
+    if tile[1] < r:
+      tile[1] += 1
+
+func _try_clear():
+  for r in height:
+    if _is_row_filled(r):
+      _clear_row(r)
+
 func _ready():
   for c in width:
     for r in height:
       var tile = BackgroundTile.instance()
+      tile.name = "Background"
       tile.position.x = c * tile_width
       tile.position.y = r * tile_width
       add_child(tile)
